18779aa30d05dc97be56cbe0a38f7feda4ccd98c
”高浜 <takahama.sin.04130413@gmail.com>
2020-02-22 12:37:11 +0000

    kadai_11_kari_commit_t5_fin

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -8,3 +8,22 @@
         <title>MyNews</title>
     </head>
 </html>
+
+
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.admin')
+
+
+{{-- profile.blade.phpの@yield('title')に'Myプロフィール'を埋め込む --}}
+@section('title', 'Myプロフィール')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
