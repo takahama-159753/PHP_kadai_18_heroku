ab02e2844f6937881fc9b8c83ce4e9ef1d761380
”高浜 <takahama.sin.04130413@gmail.com>
2020-02-25 08:50:23 +0000

    kadai_12_kari_t8

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,11 @@
 */
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::get('news/create', 'Admin\NewsController@add');
 });
 
 Route::get('admin/profile/create','Admin\ProfileController@add');
-Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
+Route::get('admin/profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 
 
 Auth::routes();
