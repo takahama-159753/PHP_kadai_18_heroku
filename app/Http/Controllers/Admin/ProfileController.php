d7140ec1f2c504add3143af2ee155d7bf079b3a8
”高浜 <takahama.sin.04130413@gmail.com>
2020-03-03 17:12:56 +0000

    kadai_14_kari_t6_fin

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     public function add()
