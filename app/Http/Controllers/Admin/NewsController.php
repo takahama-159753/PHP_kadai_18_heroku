2d6cedb6fb9cdc6a2b2ad0a2ff3a4dfb2e414990
”高浜 <takahama.sin.04130413@gmail.com>
2020-03-03 00:09:09 +0000

    kadai_14_kari_t3_fin

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -2,7 +2,6 @@
 namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
 use App\News;
 
 class NewsController extends Controller
--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        '森本', 'morimoto@tech.com', 'Hash::make(“pass”)',
     ];
 
     /**
