a7b007b6c92c79e8f7f7a43c1b1416bb783b6aae
”高浜 <takahama.sin.04130413@gmail.com>
2020-03-05 12:59:06 +0000

    kadai_14_kari_t9_fin

--- routes/web.php
+++ routes/web.php
@@ -16,26 +16,13 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
      Route::post('news/create', 'Admin\NewsController@create'); 
 });
 
-Route::get('admin/profile/create','Admin\ProfileController@add');
-Route::get('admin/profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
-
-
-///下記はPHP/Laravel 13の3番です。
-Route::group(['prefix' => 'admin'], function(){
-    Route::get('aaa/create', 'Admin\AAAController@bbb');
-});
-
-
-//下記はPHP/Laravel 13の4番です。
+//下記はprofile画面Route情報です。
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
-     Route::get('admin/profile/create','Admin\ProfileController@add');   
-     Route::post('admin/profile/create','Admin\ProfileController@create');
-     Route::post('admin/profile/edit', 'Admin\ProfileController@update');//←はPHP/Laravel 13の6番です。
+     Route::get('profile/create','Admin\ProfileController@add');   
+     Route::post('profile/create','Admin\ProfileController@create');
+     Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 
-
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
