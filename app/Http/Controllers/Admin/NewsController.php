28e4e9fef2722e116d6fb96147408e95b37e9702
”高浜 <takahama.sin.04130413@gmail.com>
2020-03-09 10:53:07 +0000

    kadai_16_kari_t4_fin

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,22 +5,19 @@ use App\Http\Controllers\Controller;
 use App\News;
 
 class NewsController extends Controller
-{
+     {
   public function add()
-  {
+     {
       return view('admin.news.create');
-  }
+     }
 
  public function create(Request $request)
-  {
-
-      
+     {
       $this->validate($request, News::$rules);
 
       $news = new News;
       $form = $request->all();
 
-      
       if (isset($form['image'])) {
         $path = $request->file('image')->store('public/image');
         $news->image_path = basename($path);
@@ -34,39 +31,35 @@ class NewsController extends Controller
       $news->fill($form);
       $news->save();
       return redirect('admin/news/create');
-      
-      
-  }
+      }
 
   public function index(Request $request)
-  {
+     {
       $cond_title = $request->cond_title;
       if ($cond_title != '') {
-          
+
           $posts = News::where('title', $cond_title)->get();
       } else {
-          
           $posts = News::all();
       }
       return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
-  }
+      }
 
 
 public function edit(Request $request)
-  {
-    
+      {
       $news = News::find($request->id);
       if (empty($news)) {
         abort(404);    
       }
       return view('admin.news.edit', ['news_form' => $news]);
-  }
+      }
 
 public function update(Request $request)
-  {
+      {
       
       $this->validate($request, News::$rules);
-      
+
       $news = News::find($request->id);
       
       $news_form = $request->all();
@@ -83,5 +76,5 @@ public function update(Request $request)
       $news->fill($news_form)->save();
 
       return redirect('admin/news');
-  }
+      }
 }
--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -26,16 +26,16 @@
 
 {{-- この章の後半で作成するCSSを読み込みます --}}
 <link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet">
- </head>
+</head>
  
  <body>
      <div id="app">
      {{-- 画面上部に表示するナビゲーションバーです。 --}}
      <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
-         <div class="container">
-         <a class="navbar-brand" href="{{ url('/') }}">
-         {{ config('app.name', 'Laravel') }}
-        </a>
+     <div class="container">
+     <a class="navbar-brand" href="{{ url('/') }}">
+     {{ config('app.name', 'Laravel') }}
+     </a>
  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
      <span class="navbar-toggler-icon"></span>
      </button>
