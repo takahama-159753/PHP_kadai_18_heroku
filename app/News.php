65ba0600d0711caca66018b98f679014c20a389a
”高浜 <takahama.sin.04130413@gmail.com>
2020-03-25 08:57:16 +0000

    kadai_17_t1

--- app/News.php
+++ app/News.php
@@ -7,11 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class News extends Model
 {
     protected $guarded = array('id');
-    
+
+  
     public static $rules = array(
         'title' => 'required',
         'body' => 'required',
     );
+
     
     public function histories()
     {
