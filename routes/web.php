ee580a60d5f83ceb0bf844ebb0f87c36be3eb97e
”高浜 <takahama.sin.04130413@gmail.com>
2020-03-31 12:48:47 +0000

    kadai_18_t1

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
      Route::get('news/edit', 'Admin\NewsController@edit'); 
      Route::post('news/edit', 'Admin\NewsController@update');
      Route::get('news/delete', 'Admin\NewsController@delete');
+     Route::get('/', 'NewsController@index');
 });
 
 //下記はprofile画面Route情報です。
