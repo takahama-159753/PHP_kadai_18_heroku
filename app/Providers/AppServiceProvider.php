d1ecc4386bd30b9dc28e3dbb853c01346ad7ed4d
”高浜 <takahama.sin.04130413@gmail.com>
2020-04-02 05:28:28 +0000

    first commit_kadai_heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,23 +6,27 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+/**
+ * Register any application services.
+ *
+ * @return void
+ */
+public function register()
+{
+    //
+}
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
+/**
+ * Bootstrap any application services.
+ *
+ * @return void
+ */
+public function boot()
+{
+// 以下を追記
+if (\App::environment('production')) {
     \URL::forceScheme('https');
-    }
+}
+}
+
 }
